e1e039043696bc837ee45c86be4d1ad75956b375
siuuuufuuuu <siuuuufuuuu@siufuusai.com>
2026-06-22 17:53:55 +0900

    - Recompile several files
    - Add a program that estimates the appropriate light level to determine whether a flashlight is shining on an object

---
+++ displayColour.cpp
@@ -0,0 +1,128 @@
+#include "SerialBridge.h"
+#include <windows.h>
+
+const char CLASS_NAME[] = "RedScreenWindow";
+
+bool showRed = false;
+DWORD redUntil = 0;
+DWORD ignoreSuccessUntil = 0;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_KEYDOWN:
+        if (wParam == VK_ESCAPE) {
+            PostQuitMessage(0);
+        }
+        return 0;
+
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        RECT rect;
+        GetClientRect(hwnd, &rect);
+
+        HBRUSH brush;
+
+        if (showRed) {
+            brush = CreateSolidBrush(RGB(255, 0, 0));
+        } else {
+            brush = CreateSolidBrush(RGB(0, 0, 0));
+        }
+
+        FillRect(hdc, &rect, brush);
+        DeleteObject(brush);
+
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    if (!openSerial()) {
+        MessageBox(nullptr, "COM3 open failed", "Error", MB_OK);
+        return 1;
+    }
+
+    HWND hwnd =
+        CreateWindowEx(0, CLASS_NAME, "Red Screen", WS_POPUP, 0, 0, GetSystemMetrics(SM_CXSCREEN),
+                       GetSystemMetrics(SM_CYSCREEN), nullptr, nullptr, hInstance, nullptr);
+
+    ShowWindow(hwnd, SW_SHOWMAXIMIZED);
+
+    auto triggerSuccess = [&]() {
+        showRed = true;
+        redUntil = GetTickCount() + 10000;
+
+        InvalidateRect(hwnd, nullptr, TRUE);
+    };
+
+    auto resetDisplay = [&]() {
+        showRed = false;
+        redUntil = 0;
+
+        InvalidateRect(hwnd, nullptr, TRUE);
+    };
+
+    MSG msg = {};
+
+    bool successKeyWasDown = false;
+    bool resetKeyWasDown = false;
+
+    while (true) {
+        while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_QUIT) {
+                return 0;
+            }
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        if (receivedSuccess() && GetTickCount() > ignoreSuccessUntil) {
+            triggerSuccess();
+        }
+
+        bool successKeyDown = (GetAsyncKeyState('U') & 0x8000) || (GetAsyncKeyState('F') & 0x8000);
+
+        bool resetKeyDown = (GetAsyncKeyState('B') & 0x8000);
+
+        if (successKeyDown && !successKeyWasDown) {
+            triggerSuccess();
+        }
+
+        if (resetKeyDown && !resetKeyWasDown) {
+            resetDisplay();
+
+            // Bキー押下後3秒間は
+            // シリアルからのSuccessを無視
+            ignoreSuccessUntil = GetTickCount() + 3000;
+        }
+
+        successKeyWasDown = successKeyDown;
+        resetKeyWasDown = resetKeyDown;
+
+        if (showRed && GetTickCount() > redUntil) {
+            showRed = false;
+
+            InvalidateRect(hwnd, nullptr, TRUE);
+        }
+
+        Sleep(10);
+    }
+}
